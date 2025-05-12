8131b2b5c34f181df88d56eb848f3b35fa96dbc6
davide-delgiudice <dav.del.giudice@gmail.com>
2025-05-12 15:40:48 +0200

    [UPD] installed react and vite, and add films array

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import films from './data/films'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Movie Filter</h1>
+    </>
+  )
+}
+
+export default App
